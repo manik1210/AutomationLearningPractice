02e83719de80d914a0e2a691a97a52784cbd5f77
manikandan <manigane12@gmail.com>
2023-07-25 14:38:59 +0530

    ArrayList and Static Variable updated

--- src/main/java/resoruces/DifferenceBetweenArrayListandLinkedList.java
+++ src/main/java/resoruces/DifferenceBetweenArrayListandLinkedList.java
@@ -2,7 +2,18 @@ package resoruces;
 
 public class DifferenceBetweenArrayListandLinkedList {
 
+    //Static Varibale can be used for all the instances in a claass,
+    //can be called with class name
+    // It can be used ti store the URLs for the Application under test
+    //Need to use only when they are absolutely necessary.
+
     static int a=10;
+
+
+    //Final Key word can be used to store constants
+    //like Urls and credentials for the Applications under test
+
+    final int b=5;
 
 
 }
--- src/main/java/resoruces/exceutionClass.java
+++ src/main/java/resoruces/exceutionClass.java
@@ -1,6 +1,7 @@
 package resoruces;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,17 +12,23 @@ public class exceutionClass {
         //To demostrate the Static Variable uses
         System.out.println(DifferenceBetweenArrayListandLinkedList.a);
 
+
         //To demonstrate the ArrayList Implementation
         List<Integer> al= new ArrayList<>();
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the Numbers ");
+        System.out.print("Enter the Numbers ");
         int n = sc.nextInt();
-        System.out.println("Enter the numbers in ArrayList :");
+        System.out.print("Enter the numbers in ArrayList :");
         for(int i=0;i<n;i++){
             al.add(sc.nextInt());
         }
 
         System.out.println("The Elements in the ArrayList :"+al);
+
+
+        //LinkedList
+        List<Integer> ll= new LinkedList<>();
+
 
 
 
